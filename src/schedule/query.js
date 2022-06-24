0665dfbf77a5d4ccbfd32a558b239379b749cd82
Lalithsaran99 <Lalithsaran99>
2022-06-24 12:07:23 +0530

    query added

---
+++ src/schedule/query.js
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client/core";
+export const schedule = gql`
+  query NewSchedule($type: String, $status: String, $page: Int) {
+    newSchedule(type: $type, status: $status, page: $page) {
+      seriesName
+      seriesID
+      matchType
+      league
+      seriesView
+      seriesAvailable
+      matches {
+        teamsWinProbability {
+          homeTeamShortName
+          homeTeamPercentage
+          awayTeamShortName
+          awayTeamPercentage
+          tiePercentage
+        }
+        matchScore {
+          teamShortName
+          teamID
+          teamFullName
+          teamScore {
+            inning
+            inningNumber
+            battingTeam
+            runsScored
+            wickets
+            overs
+            runRate
+            battingSide
+            teamID
+            battingTeamShortName
+            declared
+            folowOn
+          }
+        }
+        homeTeamName
+        awayTeamName
+      }
+    }
+  }
+`;
